4da56fc2994ce250bb37623db103f3484053f0c0
gabrielllrDev <comercial.gabrielleao@gmail.com>
2026-01-24 01:26:43 -0300

    Animaação e Lógica de Seleção de Cartas

--- Assets/Scripts/Menu/botaoCarta.cs
+++ Assets/Scripts/Menu/botaoCarta.cs
@@ -10,13 +10,37 @@ public class botaoCarta : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 	public int cartaCorrespondente;
 	public selectCartas scriptSelecao;
 
+	public static bool selectingCarta;
+	public Animator anim;
+	public statusCarta statusDaCarta;
+
+	public static statusCarta statusDaCartaNS; //NS = Não selecionada
+
+	public static bool disableNonSelectedAnimations;
+
+	public static int posicaoCartaTroca;
+	public static int posicaoCartaSelecionadaTroca;
+
 	// Use this for initialization
 	void Start () {
+
+		selectingCarta = false;
+		disableNonSelectedAnimations = false;
+
+		posicaoCartaTroca = 1;
+		posicaoCartaSelecionadaTroca = 1;
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (disableNonSelectedAnimations) {
+
+			anim.SetBool ("selectingCarta", false);
+
+
+		}
 
 		if (isInPlace) {
 
@@ -24,10 +48,82 @@ public class botaoCarta : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 
 				scriptSelecao.trocaView (cartaCorrespondente);
 
+				//if (!statusDaCarta.isSelected && !selectingCarta) {
+
+				//	selectingCarta = true;
+				//	posicaoCartaTroca = statusDaCarta.positionNumber;
+				//	anim.SetBool ("selectingCarta", true);
+
+				//	statusDaCartaNS = statusDaCarta;
+				//}
+
+				if (!statusDaCarta.isSelected && selectingCarta) {
+					
+					disableNonSelectedAnimations = true;
+					StartCoroutine (desativaEAtivaBooleana ());
+
+				}
+
+				if (statusDaCarta.isSelected && selectingCarta) {
+
+					selectingCarta = false;
+					disableNonSelectedAnimations = true;
+
+					statusDaCarta.realizaTroca = true;
+					statusDaCartaNS.realizaTroca = true;
+
+					posicaoCartaSelecionadaTroca = statusDaCarta.positionNumber;
+
+					StartCoroutine (desativaBooleana ());
+
+				}
+
+
+
 			}
 
 		}
 		
+	}
+
+	void LateUpdate(){
+
+		if (isInPlace) {
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				scriptSelecao.trocaView (cartaCorrespondente);
+
+				if (!statusDaCarta.isSelected && !selectingCarta) {
+
+					selectingCarta = true;
+					posicaoCartaTroca = statusDaCarta.positionNumber;
+					anim.SetBool ("selectingCarta", true);
+
+					statusDaCartaNS = statusDaCarta;
+				}
+
+			}
+
+		}
+
+	}
+
+	IEnumerator desativaBooleana(){
+
+		yield return new WaitForSeconds (0.0001f);
+		disableNonSelectedAnimations = false;
+
+	}
+
+	IEnumerator desativaEAtivaBooleana(){
+
+		yield return new WaitForSeconds (0.0001f);
+		disableNonSelectedAnimations = false;
+		posicaoCartaTroca = statusDaCarta.positionNumber;
+		statusDaCartaNS = statusDaCarta;
+		anim.SetBool ("selectingCarta", true);
+
 	}
 
 	public void OnPointerEnter(PointerEventData eventData){
---
+++ Assets/Scripts/Menu/statusCarta.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statusCarta : MonoBehaviour {
+
+	public bool isSelected;
+
+	public Vector3[] selectedPos;
+	public Vector3[] nonSelectedPos;
+
+	public int positionNumber;
+
+	Animator anim;
+
+	public bool realizaTroca;
+
+	float i;
+	float trocaSpeed = 5f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		i = 0;
+
+		realizaTroca = false;
+
+		anim = GetComponent<Animator> ();
+
+		if (isSelected) {
+
+			this.transform.localPosition = new Vector3 (selectedPos [positionNumber - 1].x, selectedPos [positionNumber - 1].y, selectedPos [positionNumber - 1].z);
+
+		} 
+
+		else {
+
+			this.transform.localPosition = new Vector3(nonSelectedPos [positionNumber - 1].x, nonSelectedPos [positionNumber - 1].y, nonSelectedPos [positionNumber - 1].z);
+
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (isSelected) {
+
+			this.transform.localPosition = Vector3.Lerp (selectedPos [positionNumber - 1], nonSelectedPos [botaoCarta.posicaoCartaTroca - 1], i);
+
+		} 
+
+		else {
+
+			this.transform.localPosition = Vector3.Lerp (nonSelectedPos [positionNumber - 1], selectedPos [botaoCarta.posicaoCartaSelecionadaTroca - 1], i);
+
+		}
+			
+		i = Mathf.Clamp (i, 0, 1);
+
+		if (isSelected) {
+
+			anim.SetBool ("selectingCarta", botaoCarta.selectingCarta);
+
+		}
+
+		if (realizaTroca) {
+
+			trocaPosicoes ();
+
+		} 
+
+		else {
+
+			//i = 0;
+
+		}
+		
+	}
+
+	void trocaPosicoes(){
+
+		if (i < 1) {
+
+			i = i + Time.deltaTime * trocaSpeed;
+
+		} 
+
+		else {
+
+			if (isSelected) {
+
+
+				isSelected = false;
+				i = 0;
+				positionNumber = botaoCarta.posicaoCartaTroca;
+
+			} 
+
+			else {
+
+				isSelected = true;
+				i = 0;
+				positionNumber = botaoCarta.posicaoCartaSelecionadaTroca;
+
+			}
+
+			realizaTroca = false;
+
+		}
+
+	}
+
+
+}
