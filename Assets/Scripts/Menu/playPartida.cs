6655828d3fc0e48412c63be6f816e556abfffc96
gabrielllrDev <comercial.gabrielleao@gmail.com>
2026-01-26 10:37:40 -0300

    Cutscene

---
+++ Assets/Scripts/Menu/playPartida.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class playPartida : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+
+	bool sensePresence;
+
+	public Animator cutsceneAnim;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+		if (sensePresence) {
+
+			if (Input.GetMouseButtonDown (0)) {
+
+				cutsceneAnim.SetBool ("Play", true);
+
+			}
+
+		}
+
+	}
+
+	public void OnPointerEnter(PointerEventData eventData){
+
+		sensePresence = true;
+
+	}
+
+	public void OnPointerExit(PointerEventData eventData){
+
+		sensePresence = false;
+
+	}
+}
